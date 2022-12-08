b05099b5d53f32cf66ffbaa87c6e26fe3a4a8502
Kevin Olvera <kevinolvera@Kevins-Air.hsd1.in.comcast.net>
2022-12-08 13:21:29 -0500

    initial commit

---
+++ src/MainList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+/*PARENT COMPONENT */
+const MainList = () => {
+  return (
+    <div>
+      <h1>You are Logged in!</h1>
+      <p>
+        Here are the coordinates to the hidden treasure: 41.703054, -86.238975{" "}
+      </p>
+    </div>
+  );
+};
+
+export default MainList;
---
+++ src/environments.js
@@ -0,0 +1,7 @@
+module.exports = {
+      APPLICATION_ID:
+      "YyWsxuaVpnAZh6oXdfH1wxKt8dund0ARKQ4xvdSd",
+      JAVASCRIPT_KEY:
+      "MOOJ9WNrVXgVFEC9GuwXMJMXLJxNvaEfytNygUBb",
+      SERVER_URL: "https://parseapi.back4app.com/"
+    };
